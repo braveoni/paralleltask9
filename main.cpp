cc1213d48ddc99a1b47e96eeac4345e3d65da8ed
braveoni <rfvxfnrf123>
2022-03-09 19:22:55 +0300

    Fix baseline

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include <vector>
 #include <omp.h>
+
+#define n 1000
 
 using namespace std;
 
@@ -8,16 +9,30 @@ void parallel_mul(const unsigned* X, const unsigned* Y, size_t N, unsigned* Z) {
     unsigned carry = 0;
     for (int j = (int)N - 1; j >= 0; --j)
         for (int i = (int)N - 1; i >= 0; --i) {
-            carry = Z[i + j + 1] + X[i] * Y[j] + carry;
+            carry = Z[i + j + 1] ? Z[i + j + 1] + X[i] * Y[j] + carry : X[i] * Y[j] + carry;
             Z[i + j + 1] = carry % 10;
             carry /= 10;
         }
-    cout << endl;
 }
 
-
 int main() {
-    unsigned X[3] = {1, 2, 3}, Y[3] = {3, 2, 1}, Z[6] = {0};
-    parallel_mul(X, Y, 3, Z);
-    for (auto digit: Z) cout << digit << ' ';
+    unsigned X[n], Y[n], Z[2 * n - 1];
+
+    for (auto &digit: Z) digit = 0;
+
+    for (unsigned int & i : X) {
+        i = rand() % 10;
+        cout << i;
+    }
+
+    cout << " * ";
+
+    for (unsigned int & i : Y) {
+        i = rand() % 10;
+        cout << i;
+    }
+
+    cout << endl << "============================================ " << endl;
+    parallel_mul(X, Y, n, Z);
+    for (auto digit: Z) cout << digit;
 }
