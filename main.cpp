5d16444b125ec9d03b891636b66828147bece54c
braveoni <rfvxfnrf123>
2022-03-09 18:49:21 +0300

    Baseline solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+void parallel_mul(const unsigned* X, const unsigned* Y, size_t N, unsigned* Z) {
+    unsigned carry = 0;
+    for (int j = (int)N - 1; j >= 0; --j)
+        for (int i = (int)N - 1; i >= 0; --i) {
+            carry = Z[i + j + 1] + X[i] * Y[j] + carry;
+            Z[i + j + 1] = carry % 10;
+            carry /= 10;
+        }
+    cout << endl;
+}
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    unsigned X[3] = {1, 2, 3}, Y[3] = {3, 2, 1}, Z[6] = {0};
+    parallel_mul(X, Y, 3, Z);
+    for (auto digit: Z) cout << digit << ' ';
 }
